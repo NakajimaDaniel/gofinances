c37d7475ae42ecd483e2d9904d4be2712b1d74dd
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-11-09 19:59:19 -0300

    feat: category select modal

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
+import { Modal } from 'react-native';
 import { Button } from '../../components/Forms/Button'
+import { CategorySelectButton } from '../../components/Forms/CategorySelectButton'
 import { Input } from '../../components/Forms/Input'
 import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton'
+import { CategorySelect } from '../CategorySelect'
+
 import { 
   Container,
   Header,
@@ -17,11 +21,23 @@ import {
 export function Register() {
 
   const [transactionType, setTransactionType] = useState('');
+  const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+  const[category, setCategory] = useState({
+    key: 'category',
+    name: 'categoria',
+
+  })
 
   function handleTransactionTypeSelect(type: 'up' | 'down') {
     setTransactionType(type);
   }
 
+  function handleCloseSelectCategory() {
+    setCategoryModalOpen(false);
+  }
+  function handleOpenSelectCategory() {
+    setCategoryModalOpen(true);
+  }
 
   return (
     <Container>
@@ -37,11 +53,20 @@ export function Register() {
           <TransactionTypeButton type="up" title="income" onPress={() => handleTransactionTypeSelect('up')} isActive={transactionType === 'up'}  />
           <TransactionTypeButton type="down" title="outcome" onPress={() => handleTransactionTypeSelect('down')} isActive={transactionType === 'down'}  />
         </TransactionsTypes>
+        <CategorySelectButton title={category.name} onPress={handleOpenSelectCategory}  />
       </Fields>
 
       <Button title="Enviar" />
     </Form>
 
+    <Modal visible={categoryModalOpen}>
+      <CategorySelect 
+        category = {category}
+        setCategory = {setCategory}
+        closeSelectCategory={handleCloseSelectCategory}
+      />
+    </Modal>
+
     </Container>
-  )
+  ) 
 }
