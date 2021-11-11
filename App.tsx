770384c2d19dc8b9d206c03a7d8faa280edb969b
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-11-11 19:34:44 -0300

    feat: navigation with react-navigation

--- App.tsx
+++ App.tsx
@@ -9,6 +9,9 @@ import theme from './src/global/styles/theme';
 
 import { Register } from './src/screens/Register'
 
+import { NavigationContainer } from '@react-navigation/native'
+import { AppRoutes } from './src/routes/app.routes';
+
 export default function App() {
 
   const [fontsLoaded] = useFonts({
@@ -23,8 +26,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {/* <Dashboard /> */}
-      <Register />
+     <NavigationContainer>
+     <AppRoutes />
+     </NavigationContainer>
     </ThemeProvider>
 
   );
