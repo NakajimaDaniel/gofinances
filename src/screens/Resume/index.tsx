7e289c8cf688745b2e878b8a44c6173f801fcee0
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-11-17 19:38:55 -0300

    feat: listing expenses by category

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,17 +1,79 @@
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react';
+import { ScrollView } from 'react-native-gesture-handler';
 import { HistoryCard } from '../../components/HistoryCard';
-import { Container, Header, Title } from './styles';
+import { categories } from '../../utils/categories';
+import { Container, Header, Title, Content } from './styles';
 
+interface TransactionData {
+  type: 'positive' | 'negative';
+  name: string;
+  amount: string;
+  category: string;
+  date: string;
+}
 
+interface CategoryData {
+  key: string;
+  name: string;
+  total: string;
+  color: string;
+}
 
 export function Resume() {
+
+  const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
+
+  async function loadData() {
+    const dataKey = '@gofinances:transactions';
+    const response = await AsyncStorage.getItem(dataKey);
+    const responseFormatted = response ? JSON.parse(response) : [];
+
+    const expenses = responseFormatted.filter((expenses:TransactionData) => expenses.type === 'negative');
+
+    const totalByCategory:CategoryData[] = [];
+
+    categories.forEach(category => {
+      let categorySum = 0;
+
+      expenses.forEach((expense:TransactionData) => {
+        if(expense.category === category.key) {
+          categorySum += Number(expense.amount);
+        }
+      })
+
+      if(categorySum) {
+        const total = categorySum.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        })
+        totalByCategory.push({
+          key: category.key,
+          name: category.name,
+          total: total,
+          color: category.color
+        })
+      }
+
+    })
+    setTotalByCategories(totalByCategory);
+  }
+
+
+  useEffect(() => {
+    loadData();
+  },[])
+
   return (
     <Container>
       <Header>
         <Title>Resumo</Title>
       </Header>
-
-      <HistoryCard title="compras" amount="1500" color="red" />
+      <Content>
+        {totalByCategories.map(item => (
+          <HistoryCard key={item.key}  title={item.name} amount={item.total} color={item.color} />
+        ))}
+      </Content>
     </Container>
   )
 }
