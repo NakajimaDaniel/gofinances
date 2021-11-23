99ffdc9560202654f6dc87807716699860dd35f6
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-11-23 19:34:19 -0300

    fix: updated credentials

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,6 +1,8 @@
 import React, { createContext, ReactNode, useContext, useState } from 'react'
 import * as AuthSession from 'expo-auth-session';
 
+const { CLIENT_ID } = process.env;
+const { REDIRECT_URI } = process.env;
 interface AuthProviderProps {
   children: ReactNode;
 }
@@ -35,8 +37,7 @@ function AuthProvider({ children } : AuthProviderProps) {
   async function signInWithGoogle() {
 
     try {
-      const CLIENT_ID = '87719164511-15pffkbrr1v0p3irorfdp447qhgv1gvp.apps.googleusercontent.com'
-      const REDIRECT_URI = 'https://auth.expo.io/@hyltri/gofinances';
+
       const RESPONSE_TYPE = 'token';
       const SCOPE = encodeURI('profile email');
 
