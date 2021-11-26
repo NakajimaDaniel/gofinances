d146787b2d6f1d49d645bc591872e16c07f872c6
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-11-26 18:31:36 -0300

    fix: linking transactions data to user

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -12,6 +12,7 @@ import { addMonths, subMonths, format } from 'date-fns';
 import { ptBR } from 'date-fns/locale'
 import { ActivityIndicator } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
+import { useAuth } from '../../hooks/auth';
 
 
 interface TransactionData {
@@ -39,6 +40,8 @@ export function Resume() {
 
   const theme = useTheme();
 
+  const { user } = useAuth();
+
   function handleChangeDate(action: 'next' | 'prev') {
     if(action === 'next') {
       const newDate = addMonths(selectedDate, 1);
@@ -51,7 +54,7 @@ export function Resume() {
 
   async function loadData() {
     setIsLoading(true);
-    const dataKey = '@gofinances:transactions';
+    const dataKey = `@gofinances:transactions_user:${user.id}`;
     const response = await AsyncStorage.getItem(dataKey);
     const responseFormatted = response ? JSON.parse(response) : [];
 
